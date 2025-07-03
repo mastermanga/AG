0b5f042372625ae7aef2187e4acc301d9dbad4cc
mastermanga <34009607+mastermanga@users.noreply.github.com>
2025-07-03 15:42:19 +0200

    Update script.js

--- AnimeTournament/script.js
+++ AnimeTournament/script.js
@@ -3,10 +3,20 @@ document.getElementById("back-to-menu").addEventListener("click", function() {
   window.location.href = "../index.html"; // Remplace par ton URL du menu
 });
 
-// Bouton changer de thème
-document.getElementById("toggle-theme").addEventListener("click", function() {
+document.getElementById("themeToggle").addEventListener("click", () => {
   document.body.classList.toggle("light");
+  const isLight = document.body.classList.contains("light");
+  localStorage.setItem("theme", isLight ? "light" : "dark");
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "light") {
+    document.body.classList.add("light");
+  }
+});
+
+
 
 (() => {
   const TOTAL_ITEMS = 16;
